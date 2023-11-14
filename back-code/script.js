117f10871862b0f8cc63c4854e0a898110b27cc4
Apoorv-AK-Coder <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2023-11-14 03:30:19 +0000

    footer

--- back-code/script.js
+++
@@ -1,8 +0,0 @@
-// function myFunction() {
-//     var x = document.getElementById("myNavbar");
-//     if (x.className === "navbar") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "navbar";
-//     }
-//   }

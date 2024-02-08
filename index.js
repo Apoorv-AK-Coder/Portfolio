ad49c0db39b203787d3db3e1350dec00dd118ade
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-02-08 17:44:04 +0530

    almost ready

---
+++ index.js
@@ -0,0 +1,24 @@
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    // const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+
+    // Send email using EmailJS
+    emailjs.send("service_wkgfga4", "template_4vhwepn", {
+        name: name,
+        email: email,
+        // phone: phone,
+        message: message
+    })
+    .then(function(response) {
+        console.log("Email sent:", response);
+        alert("Email sent successfully!");
+        contactForm.reset();
+    }, function(error) {
+        console.error("Error sending email:", error);
+        alert("An error occurred while sending the email. Please try again later.");
+    });
+});

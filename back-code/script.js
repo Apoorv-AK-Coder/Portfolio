7fe3804ef4575b82438e2726c4778906dc3a4426
Apoorv-AK-Coder <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-02-10 10:41:56 +0530

    almost final design

--- back-code/script.js
+++
@@ -1,8 +0,0 @@
-let hamburgerMenu = document.querySelector(".hamburgerMenu");
-
-let navbar = document.querySelector(".navbar")
-
-hamburgerMenu.addEventListener("click", function(){
-  navbar.classList.toggle("active");
-  console.log("hello")
-})
